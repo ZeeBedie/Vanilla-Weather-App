cccdb329039b8b2e4e73b00429cbd56682b69cc5
ZeeBedie <131893782+ZeeBedie@users.noreply.github.com>
2023-10-09 11:53:04 +0100

    updated javascript with the city and weather data

--- Vanilla Weather App/src/app.js
+++ Vanilla Weather App/src/app.js
@@ -1,9 +1,28 @@
 function displayTemperature(response) {
-  console.log(response.data.main);
+  try {
+    const temperatureElement = document.querySelector('#temperatureid');
+    const cityElement = document.querySelector('#city');
+    const descriptionElement = document.querySelector('#description');
+
+    const temperature = response.data.temperature.current;
+    const cityName = response.data.city;
+    const weatherDescription = response.data.condition.description;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = cityName;
+    descriptionElement.innerHTML = weatherDescription;
+  } catch (error) {
+    console.error("Error handling API response:", error);
+  }
 }
 
 let apiKey = "b50343183o490adctc2bad01bf0a4ae0";
-let city = "NewYork";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+let city = "Lisbon";
+let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=Lisbon&key=b50343183o490adctc2bad01bf0a4ae0';
+
+axios.get(apiUrl, {
+  headers: {
+    "Authorization": `Bearer ${apiKey}`
+  }
+}).then(displayTemperature);
+
