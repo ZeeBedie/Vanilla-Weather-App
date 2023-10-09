5db66c3d79ea82e80f81feb8e7715b0912e49a3d
ZeeBedie <131893782+ZeeBedie@users.noreply.github.com>
2023-10-09 12:07:18 +0100

    Added humidity and wind speed to the html and js code

--- Vanilla Weather App/src/app.js
+++ Vanilla Weather App/src/app.js
@@ -3,14 +3,20 @@ function displayTemperature(response) {
     const temperatureElement = document.querySelector('#temperatureid');
     const cityElement = document.querySelector('#city');
     const descriptionElement = document.querySelector('#description');
+    const humidityElement = document.querySelector('#humidity');
+    const windElement = document.querySelector('#wind'); // Define windElement
 
     const temperature = response.data.temperature.current;
     const cityName = response.data.city;
     const weatherDescription = response.data.condition.description;
+    const humidity = response.data.temperature.humidity;
+    const wind = response.data.wind.speed; // Corrected wind access
 
     temperatureElement.innerHTML = Math.round(temperature);
     cityElement.innerHTML = cityName;
     descriptionElement.innerHTML = weatherDescription;
+    humidityElement.innerHTML = `Humidity: ${humidity}%`;
+    windElement.innerHTML = `Wind Speed: ${wind} m/s`; // Use template literals for wind speed
   } catch (error) {
     console.error("Error handling API response:", error);
   }
@@ -18,11 +24,10 @@ function displayTemperature(response) {
 
 let apiKey = "b50343183o490adctc2bad01bf0a4ae0";
 let city = "Lisbon";
-let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=Lisbon&key=b50343183o490adctc2bad01bf0a4ae0';
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
 axios.get(apiUrl, {
   headers: {
     "Authorization": `Bearer ${apiKey}`
   }
 }).then(displayTemperature);
-
