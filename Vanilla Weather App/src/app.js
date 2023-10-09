404bd4ad3c4270e57b6f885a3fddb033571a7761
ZeeBedie <131893782+ZeeBedie@users.noreply.github.com>
2023-10-09 10:45:19 +0100

    added API key

--- Vanilla Weather App/src/app.js
+++ Vanilla Weather App/src/app.js
@@ -1,1 +1,9 @@
-alert ("Hello World")
+function displayTemperature(response) {
+  console.log(response.data.main);
+}
+
+let apiKey = "b50343183o490adctc2bad01bf0a4ae0";
+let city = "NewYork";
+let apiUrl = "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
