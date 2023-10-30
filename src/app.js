138fee57cbc0566ab5d8cc793f45f83fd7624c74
ZeeBedie <131893782+ZeeBedie@users.noreply.github.com>
2023-10-30 16:55:20 +0000

    Showing the current temperature , city, humidity and wind speed

--- src/app.js
+++ src/app.js
@@ -1,82 +1,19 @@
 function displayTemperature(response) {
-  try {
-    const temperatureElement = document.querySelector("#temperatureid");
-    const cityElement = document.querySelector("#city");
-    const descriptionElement = document.querySelector("#description");
-    const humidityElement = document.querySelector("#humidity");
-    const windElement = document.querySelector("#wind");
-    const dateElement = document.querySelector("#date");
-    const iconElement = document.querySelector("#icon");
+  console.log(apiUrl);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector ("#humidity");
+  let windElement=document.querySelector ("#wind");
+  temperatureElement.innerHTML = Math.round (response.data.temperature.current);
+  cityElement.innerHTML= response.data.city;
+  descriptionElement.innerHTML=response.data.condition.description;
+  humidityElement.innerHTML=response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 
-    const temperature = response.data.temperature.current;
-    const cityName = response.data.city;
-    const weatherDescription = response.data.condition.description;
-    const humidity = response.data.temperature.humidity;
-    const wind = response.data.wind.speed;
-    const iconUrl = response.data.condition.icon_url;
-
-    const currentDate = new Date();
-    const options = {
-      weekday: "long",
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    };
-
-    const formattedDate = currentDate.toLocaleString(undefined, options);
-
-    temperatureElement.innerHTML = Math.round(temperature);
-    cityElement.innerHTML = cityName;
-    descriptionElement.innerHTML = weatherDescription;
-    humidityElement.innerHTML = `${humidity}%`;
-    windElement.innerHTML = `${wind} m/s`;
-    dateElement.innerHTML = formattedDate;
-    iconElement.setAttribute("src", iconUrl);
-  } catch (error) {
-    console.error("Error handling API response:", error);
-  }
 }
 
-function displayForecast() {
-  const forecastElement = document.querySelector("#forecast");
-
-  forecastElement.innerHTML = `
-  <div class="col-md-2">
-    <div class="weather-forecast-date">
-      Thurs
-    </div>
-    <img src="images/rain-image.png" alt="raining" width="22px" />
-    <div class="weather-forecast-temperature">
-      <span class="weather-forecast-temperature-maximum">
-        18&deg
-      </span>
-      <span class="weather-forecast-temperature-minimum">
-        12&deg
-      </span>
-    </div>
-  </div>
-`;
-}
-displayForecast();
-
-function search(event) {
-  event.preventDefault();
-  const cityInputElement = document.querySelector("#city-input");
-  const apiKey = "b50343183o490adctc2bad01bf0a4ae0";
-  const city = cityInputElement.value;
-  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-  displayForecast(); // Call the displayForecast function
-
-  axios
-    .get(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    })
-    .then(displayTemperature);
-}
-
-// Attach the event listener to the form
-const form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
+let aiKey ="b50343183o490adctc2bad01bf0a4ae0";
+let apiUrl='https://api.shecodes.io/weather/v1/current?query=London&key=b50343183o490adctc2bad01bf0a4ae0&units=metric';
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
